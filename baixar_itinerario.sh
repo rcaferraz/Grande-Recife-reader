64cb2223495d8e58417c542ece6aeeb7af71d3c2
Rodolfo Ferraz <rcaferraz@gmail.com>
2012-08-18 19:42:45 -0300

    baixar_itinerario.sh: no caso de nao ter parametros suficientes, a impressao das instrucoes sai no stderr

--- baixar_itinerario.sh
+++ baixar_itinerario.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ $# -lt 2 ]; then
-	echo "Este programa requer dois parametros: o numero da linha e o numero do itinerario."
-	echo ""
-	echo "Exemplo:"
-	echo "         $0 522 8012" 
+	echo "Este programa requer dois parametros: o numero da linha e o numero do itinerario." >&2
+	echo "" >&2
+	echo "Exemplo:" >&2
+	echo "         $0 522 8012" >&2
 	exit
 fi
 echo "Baixando http://200.238.84.28/site/consulta/itinerarios.asp?linha=`echo $1`&nomeitinerario=`echo $2`"
