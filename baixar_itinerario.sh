dc21d711c5ec8908d5edf75acaec4e4086b91177
Rodolfo Ferraz <rcaferraz@gmail.com>
2012-08-18 14:16:49 -0300

    gerar_tabela_itinerarios.sh: dividindo o codigo em funcoes. baixar_itinerario.sh: checa se recebe dois parametros antes de executar.

--- baixar_itinerario.sh
+++ baixar_itinerario.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+	echo "Este programa requer dois parametros: o numero da linha e o numero do itinerario."
+	echo ""
+	echo "Exemplo:"
+	echo "         $0 522 8012" 
+	exit
+fi
 echo "Baixando http://200.238.84.28/site/consulta/itinerarios.asp?linha=`echo $1`&nomeitinerario=`echo $2`"
 wget "http://200.238.84.28/site/consulta/itinerarios.asp?linha=`echo $1`&nomeitinerario=`echo $2`" --output-document="itinerario_`echo $1`_`echo $2`_raw"
