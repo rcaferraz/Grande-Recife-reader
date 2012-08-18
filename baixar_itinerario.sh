2d9544f5c48ede477a1db87051576a0380792e49
Rodolfo Ferraz <rcaferraz@gmail.com>
2012-08-18 12:35:32 -0300

    renomeando novamente

--- baixar_itinerario.sh
+++ baixar_itinerario.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "http://200.238.84.28/site/consulta/itinerarios.asp?linha=`echo $1`&nomeitinerario=`echo $2`"
+echo "Baixando http://200.238.84.28/site/consulta/itinerarios.asp?linha=`echo $1`&nomeitinerario=`echo $2`"
 wget "http://200.238.84.28/site/consulta/itinerarios.asp?linha=`echo $1`&nomeitinerario=`echo $2`" --output-document="itinerario_`echo $1`_`echo $2`_raw"
