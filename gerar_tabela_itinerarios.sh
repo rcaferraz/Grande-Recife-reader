338195e3cef09b6a0118dc2d1c1da97fcf23e4d6
Rodolfo Ferraz <rcaferraz@gmail.com>
2012-08-22 21:07:15 -0300

    Mudando extensao de arquivos de saida do gerar_tabela_itinerarios.sh e aparentemente ha um novo itinerario no site.

--- gerar_tabela_itinerarios.sh
+++ gerar_tabela_itinerarios.sh
@@ -7,12 +7,12 @@ baixar_pagina ()
 
 extrair_lista_options ()
 {
-	grep --regexp="<option\svalue\s*=\s*" itinerarios.asp > linhas_onibus_raw 
+	grep --regexp="<option\svalue\s*=\s*" itinerarios.asp > linhas_onibus_raw.txt 
 }
 
 remover_caracteres_invalidos ()
 {
-	tr -cd '\11\12\15\40-\176' < linhas_onibus_raw > linhas_onibus_raw_ascii
+	tr -cd '\11\12\15\40-\176' < linhas_onibus_raw.txt > linhas_onibus_raw_ascii
 }
 
 remover_tags_html ()
@@ -22,12 +22,12 @@ remover_tags_html ()
 
 extrair_lista_itinerarios ()
 {
-	grep --regexp="arrayitinerario\[[[:digit:]]\+\]\[[[:digit:]]\+\]" itinerarios.asp > itinerarios_raw
+	grep --regexp="arrayitinerario\[[[:digit:]]\+\]\[[[:digit:]]\+\]" itinerarios.asp > itinerarios_raw.txt
 }
 
 remover_javascript ()
 {
-	sed s/"\(arrayitinerario\[2\]\[[[:digit:]]\+\][[:space:]]*=[[:space:]]*'[[:alnum:]]*';\)"/"\1\n"/g itinerarios_raw > itinerarios_raw2
+	sed s/"\(arrayitinerario\[2\]\[[[:digit:]]\+\][[:space:]]*=[[:space:]]*'[[:alnum:]]*';\)"/"\1\n"/g itinerarios_raw.txt > itinerarios_raw2
 	sed s/"arrayitinerario\[[[:digit:]]\+\]\[[[:digit:]]\+\][[:space:]]*=[[:space:]]*"//g itinerarios_raw2 > itinerarios_raw3
 	sed s/\'//g itinerarios_raw3 | sed s/";"/":"/g | sed s/":\$"// | sort > itinerarios_sem_as_linhas
 }
@@ -39,9 +39,9 @@ gerar_lista_itinerarios ()
 
 limpeza ()
 {
-	rm itinerarios_raw itinerarios_raw2 itinerarios_raw3 itinerarios.asp
-	rm linhas_onibus_raw linhas_onibus_raw_ascii
-	rm itinerarios_sem_as_linhas linhas_onibus
+	rm itinerarios_raw.txt itinerarios_raw2 itinerarios_raw3 itinerarios.asp
+	rm linhas_onibus_raw.txt linhas_onibus_raw_ascii
+#	rm itinerarios_sem_as_linhas linhas_onibus
 }
 
 
