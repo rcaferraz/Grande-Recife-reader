7da7859ad7b6f35c356aeb412d2e10bf900ce6e9
Rodolfo Ferraz <rcaferraz@gmail.com>
2012-08-24 00:25:50 -0300

    itinerarios_clean_to_sql.sh: so' precisa resolver o sed que nao reconhece os caracteres em portugues (til, cedilha, etc). eles nao dao match com simbolo . (qualquer caractere)

--- 3.0-itinerarios_clean_to_sql.sh
+++ 3.0-itinerarios_clean_to_sql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep . --with-filename --line-number itinerarios_clean/* | sed s/itinerarios_clean\\/// > testeOUT 
+grep . --with-filename --line-number itinerarios_clean/* | sed s/"itinerarios_clean\\/itinerario_[[:digit:]]\+_\([[:digit:]]\+\)_clean:\([[:digit:]]\+\):\(.\+\)"/"\1:\2:\3"/ > testeOUT 
