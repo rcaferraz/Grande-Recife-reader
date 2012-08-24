13d30ec0e0997104c1dadf9eda651bb50e1e5c08
Rodolfo Ferraz <rcaferraz@gmail.com>
2012-08-24 00:42:14 -0300

    itinerarios_clean_to_sql.sh: corrigindo problema com caracteres brasileiros.

--- 3.0-itinerarios_clean_to_sql.sh
+++ 3.0-itinerarios_clean_to_sql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep . --with-filename --line-number itinerarios_clean/* | sed s/"itinerarios_clean\\/itinerario_[[:digit:]]\+_\([[:digit:]]\+\)_clean:\([[:digit:]]\+\):\(.\+\)"/"\1:\2:\3"/ > testeOUT 
+grep . --with-filename --line-number itinerarios_clean/* | iconv -f latin1 -t utf-8 | sed s/"itinerarios_clean\\/itinerario_[[:digit:]]\+_\([[:digit:]]\+\)_clean:\([[:digit:]]\+\):\(.\+\)"/"\1:\2:\3"/ > testeOUT 
