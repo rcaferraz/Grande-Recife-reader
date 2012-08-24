ca45e0bf73358bdbd483cc859aa61b1e104ca744
Rodolfo Ferraz <rcaferraz@gmail.com>
2012-08-24 01:04:14 -0300

    modificacoes relacionadas ao suporte de caracteres brasileiros, que agora tenho

--- 0.2-itinerarios_to_sql.sh
+++ 0.2-itinerarios_to_sql.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sed s/"\([[:digit:]]\+\):\([[:digit:]]\+\):\([[:alnum:]]\+\):.\+"/"INSERT INTO \`onibusrecife\`.\`searchstreet_itinerario\` ( \`numero\` , \`nome\` , \`onibus_id\` ) VALUES ( '\2', '\3', '\1');"/ lista_itinerarios.txt
+iconv -f latin1 -t utf-8 < lista_itinerarios.txt | sed s/"\([[:digit:]]\+\):\([[:digit:]]\+\):\([[:alnum:]]\+\):.\+"/"INSERT INTO \`onibusrecife\`.\`searchstreet_itinerario\` ( \`numero\` , \`nome\` , \`onibus_id\` ) VALUES ( '\2', '\3', '\1');"/
+	
