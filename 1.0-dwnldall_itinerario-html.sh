1db201f61a102f1013918fe9c0d900c663fcbb16
Rodolfo Ferraz <rcaferraz@gmail.com>
2012-08-24 09:19:36 -0300

    versao completa

--- 1.0-dwnldall_itinerario-html.sh
+++ 1.0-dwnldall_itinerario-html.sh
@@ -6,5 +6,5 @@ do
 #	echo -e "\tlinha_nome: $linha_nome"
 #	echo -e "\titine_num: $itinerario_num"
 #	echo -e "\t\titine_nom: $itinerario_nome"
-	./baixar_itinerario.sh `echo $linha_num $itinerario_num`
+	./dwnld_itinerario-html.sh `echo $linha_num $itinerario_num`
 done < lista_itinerarios.txt
--- 3.0-itinerarios_clean_to_sql.sh
+++ 3.0-itinerarios_clean_to_sql.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-grep . --with-filename --line-number itinerarios_clean/* | iconv -f latin1 -t utf-8 | sed s/"itinerarios_clean\\/itinerario_[[:digit:]]\+_\([[:digit:]]\+\)_clean:\([[:digit:]]\+\):\(.\+\)"/"\1:\2:\3"/ > testeOUT 
+grep . --with-filename --line-number itinerarios_clean/* | iconv -f latin1 -t utf-8 | sed s/"itinerarios_clean\\/itinerario_[[:digit:]]\+_\([[:digit:]]\+\)_clean:\([[:digit:]]\+\):\(.\+\)"/"INSERT INTO \`onibusrecife\`.\`searchstreet_itinerario_parada\` (\`id\`, \`itinerario_id\`, \`parada_id\`, \`parada_literal\`, \`ordem\`) VALUES (NULL, '\1', NULL, '\3', '\2');"/ 
+
